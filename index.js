40dd80fa089318f8f0f8feced4acc56044ee33a7
zeitun-adan <zeitun.adan@student.moringaschool.com>
2022-06-06 09:16:54 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name){
+    cats.push("Ralph")
+    return cats
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob")
+    return cats
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop()
+    return cats;
+
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift()
+    return cats
+}
+function appendCat(name){
+    let cats2 = [...cats]
+    cats2.push("Broom")
+    return cats2
+}
+
+function prependCat(name){
+    let cats2 = [...cats]
+    cats2.unshift("Arnold")
+    return cats2
+}
+
+function removeLastCat(name){
+    let cats2 = [...cats]
+    cats2.pop()
+    return cats2;
+}
+function removeFirstCat(name){
+    let cats2 = [...cats]
+    cats2.shift()
+    return cats2
+}
